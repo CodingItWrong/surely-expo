acfd3d2da3216f5b61c73b7bb2333e0eb431d0e6
Josh Justice <me@codingitwrong.com>
2021-09-09 08:40:50 -0400

    Add RN Paper

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
 import Todos from './Todos';
 
 export default function App() {
   return (
-    <>
+    <PaperProvider>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <Todos />
       </SafeAreaView>
-    </>
+    </PaperProvider>
   );
 }
