f565ad21980bf4c6eadcef569b336fab7e9d4078
Josh Justice <me@codingitwrong.com>
2021-09-10 08:43:30 -0400

    Add oauth login (token not used yet)

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import Auth from './Auth';
 import NavigationBar from './NavigationBar';
 import Todo from './Todo';
 import Todos from './Todos';
@@ -28,14 +29,20 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <PaperProvider theme={theme}>
-      <NavigationContainer linking={linking}>
-        <Stack.Navigator
-          screenOptions={{header: props => <NavigationBar {...props} />}}
-        >
-          <Stack.Screen name="Todos" component={Todos} />
-          <Stack.Screen name="Todo" component={Todo} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Auth>
+        {({logOut}) => (
+          <NavigationContainer linking={linking}>
+            <Stack.Navigator
+              screenOptions={{
+                header: props => <NavigationBar logOut={logOut} {...props} />,
+              }}
+            >
+              <Stack.Screen name="Todos" component={Todos} />
+              <Stack.Screen name="Todo" component={Todo} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        )}
+      </Auth>
     </PaperProvider>
   );
 }
---
+++ Auth.js
@@ -0,0 +1,47 @@
+import {OAuth} from '@codingitwrong/react-login';
+import axios from 'axios';
+import React from 'react';
+import {Platform} from 'react-native';
+import {Appbar, Button, Text, TextInput} from 'react-native-paper';
+
+const Auth = ({children}) => (
+  <OAuth
+    initiallyLoggedIn={false}
+    httpClient={httpClient}
+    handleAccessToken={setToken}
+    renderForm={renderForm}
+    renderLoggedIn={children}
+  />
+);
+
+export default Auth;
+
+const baseURL =
+  Platform.OS === 'android' ? 'http://10.0.2.2:3000' : 'http://localhost:3000';
+
+const httpClient = axios.create({baseURL});
+const setToken = () => {}; // TODO
+
+const renderForm = ({username, password, error, handleChange, handleLogIn}) => (
+  <>
+    <Appbar.Header>
+      <Appbar.Content title="Surely" />
+    </Appbar.Header>
+    {error && <Text>{error}</Text>}
+    <TextInput
+      label="Email"
+      value={username}
+      onChangeText={handleChange('username')}
+      keyboardType="email-address"
+      autoCapitalize="none"
+      autoCorrect={false}
+    />
+    <TextInput
+      label="Password"
+      value={password}
+      onChangeText={handleChange('password')}
+      secureTextEntry
+    />
+    <Button onPress={handleLogIn}>Sign in</Button>
+  </>
+);
