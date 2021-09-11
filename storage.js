319ab0f123c7f4942288e29f86381cdf94b3fbbf
Josh Justice <me@codingitwrong.com>
2021-09-11 06:42:59 -0400

    Add storage for web

--- storage.js
+++ storage.js
@@ -1,13 +1,30 @@
 import * as SecureStore from 'expo-secure-store';
+import {Platform} from 'react-native';
+
+const isWeb = Platform.OS === 'web';
 
 export function setStringAsync(key, value) {
-  return SecureStore.setItemAsync(key, value);
+  if (isWeb) {
+    window.localStorage.setItem(key, value);
+    return Promise.resolve();
+  } else {
+    return SecureStore.setItemAsync(key, value);
+  }
 }
 
 export function getStringAsync(key) {
-  return SecureStore.getItemAsync(key);
+  if (isWeb) {
+    return Promise.resolve(window.localStorage.getItem(key));
+  } else {
+    return SecureStore.getItemAsync(key);
+  }
 }
 
 export function deleteStringAsync(key) {
-  return SecureStore.deleteItemAsync(key);
+  if (isWeb) {
+    window.localStorage.removeItem(key);
+    return Promise.resolve();
+  } else {
+    return SecureStore.deleteItemAsync(key);
+  }
 }
