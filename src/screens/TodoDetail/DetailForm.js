42f68c39b7bba8ab6a22312ab73030437ca19a8c
Josh Justice <me@codingitwrong.com>
2021-09-16 06:31:08 -0400

    Allow editing deferred date

--- src/screens/TodoDetail/DetailForm.js
+++ src/screens/TodoDetail/DetailForm.js
@@ -1,12 +1,32 @@
 import React, {useState} from 'react';
 import {Button, TextInput} from 'react-native-paper';
+import {DatePickerModal} from 'react-native-paper-dates';
+import {relativeDate} from '../../utils/time';
 
 export default function DetailForm({todo, onSave, onCancel}) {
   const [name, setName] = useState(todo.attributes.name ?? '');
   const [notes, setNotes] = useState(todo.attributes.notes ?? '');
+  const [deferredUntil, setDeferredUntil] = useState(
+    todo.attributes['deferred-until'],
+  );
+
+  const [isDeferredUntilModalOpen, setIsDeferredUntilModalOpen] =
+    useState(false);
+
+  function handleChangeDeferredUntil({date}) {
+    console.log({date});
+    if (date) {
+      console.log(1, date.toISOString());
+      setDeferredUntil(date.toISOString());
+    } else {
+      console.log(2);
+      setDeferredUntil(null);
+    }
+    setIsDeferredUntilModalOpen(false);
+  }
 
   function handlePressSave() {
-    const attributes = {name, notes};
+    const attributes = {name, notes, 'deferred-until': deferredUntil};
     onSave(attributes);
   }
 
@@ -17,6 +37,16 @@ export default function DetailForm({todo, onSave, onCancel}) {
         value={name}
         onChangeText={setName}
         multiline
+      />
+      <Button mode="outlined" onPress={() => setIsDeferredUntilModalOpen(true)}>
+        Deferred until {relativeDate(deferredUntil)}
+      </Button>
+      <DatePickerModal
+        visible={isDeferredUntilModalOpen}
+        mode="single"
+        date={deferredUntil ? new Date(deferredUntil) : null}
+        onConfirm={handleChangeDeferredUntil}
+        onDismiss={() => setIsDeferredUntilModalOpen(false)}
       />
       <TextInput
         label="Notes"
