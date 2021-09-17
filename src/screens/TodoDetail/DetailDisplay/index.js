62beccdf27b015076dab8e6b5dbf1c2b1737ba0c
Josh Justice <me@codingitwrong.com>
2021-09-17 07:00:41 -0400

    Add scroll view to detail view

--- src/screens/TodoDetail/DetailDisplay/index.js
+++ src/screens/TodoDetail/DetailDisplay/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import {ScrollView} from 'react-native';
 import {IconButton, Text, Title} from 'react-native-paper';
 import Actions from './Actions';
 import EventLog from './EventLog';
 
 export default function DetailDisplay({todo, onEdit, onUpdate, onGoBack}) {
   return (
-    <>
+    <ScrollView>
       <IconButton icon="pencil" accessibilityLabel="Edit" onPress={onEdit} />
       <Title>{todo.attributes.name}</Title>
       {todo.attributes.notes ? <Text>{todo.attributes.notes}</Text> : null}
       <EventLog todo={todo} />
       <Actions todo={todo} onUpdate={onUpdate} onGoBack={onGoBack} />
-    </>
+    </ScrollView>
   );
 }
