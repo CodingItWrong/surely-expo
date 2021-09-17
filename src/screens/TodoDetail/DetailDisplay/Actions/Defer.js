73ab75e7eeadd6c1bb6ef75d2158e62413a9827f
Josh Justice <me@codingitwrong.com>
2021-09-17 06:42:38 -0400

    Add defer until date

--- src/screens/TodoDetail/DetailDisplay/Actions/Defer.js
+++ src/screens/TodoDetail/DetailDisplay/Actions/Defer.js
@@ -1,31 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button} from 'react-native-paper';
+import {DatePickerModal} from 'react-native-paper-dates';
 import {useTodos} from '../../../../data/todos';
 import {dayOfWeek, deferDate} from '../../../../utils/time';
 
 export default function Defer({todo, onUpdate, onCancel, onComplete}) {
+  const [isDeferredUntilModalOpen, setIsDeferredUntilModalOpen] =
+    useState(false);
+
   const {id} = todo;
+  const deferredUntil = todo.attributes['deferred-until'];
   const todoClient = useTodos();
 
   const updateAttributes = attributes => todoClient.update({id, attributes});
 
   const handleResponse = ({data}) => onUpdate(data);
 
-  const deferDays = date => () =>
-    updateAttributes({'deferred-until': date})
+  const handleDeferUntil = date => {
+    console.log({date});
+    return updateAttributes({'deferred-until': date})
       .then(handleResponse)
+      .then(() => setIsDeferredUntilModalOpen(false))
       .then(onComplete)
       .catch(console.error);
+  };
 
   return (
     <>
       <Button mode="outlined" onPress={onCancel}>
         Cancel
       </Button>
-      <DeferButton todo={todo} numDays={1} label="1 Day" onDefer={deferDays} />
-      <DeferButton todo={todo} numDays={2} label="2 Days" onDefer={deferDays} />
-      <DeferButton todo={todo} numDays={3} label="3 Days" onDefer={deferDays} />
-      <DeferButton todo={todo} numDays={7} label="1 Week" onDefer={deferDays} />
+      <DeferButton
+        todo={todo}
+        numDays={1}
+        label="1 Day"
+        onDefer={handleDeferUntil}
+      />
+      <DeferButton
+        todo={todo}
+        numDays={2}
+        label="2 Days"
+        onDefer={handleDeferUntil}
+      />
+      <DeferButton
+        todo={todo}
+        numDays={3}
+        label="3 Days"
+        onDefer={handleDeferUntil}
+      />
+      <DeferButton
+        todo={todo}
+        numDays={7}
+        label="1 Week"
+        onDefer={handleDeferUntil}
+      />
+      <Button mode="outlined" onPress={() => setIsDeferredUntilModalOpen(true)}>
+        Pick Date
+      </Button>
+      <DatePickerModal
+        visible={isDeferredUntilModalOpen}
+        mode="single"
+        saveLabel="Defer"
+        date={deferredUntil ? new Date(deferredUntil) : null}
+        onConfirm={({date}) => handleDeferUntil(date)}
+        onDismiss={() => setIsDeferredUntilModalOpen(false)}
+      />
     </>
   );
 }
@@ -36,7 +75,7 @@ function DeferButton({todo, numDays, label, onDefer}) {
     days: numDays,
   });
   return (
-    <Button mode="outlined" onPress={onDefer(date)}>
+    <Button mode="outlined" onPress={() => onDefer(date)}>
       {label} - {dayOfWeek(date)}
     </Button>
   );
--- src/screens/TodoDetail/DetailDisplay/Actions/index.js
+++ src/screens/TodoDetail/DetailDisplay/Actions/index.js
@@ -5,21 +5,26 @@ import Defer from './Defer';
 const BUTTON_SET = {
   DEFAULT: 'DEFAULT',
   DEFER: 'DEFER',
-  DEFER_UNTIL_DATE: 'DEFER_UNTIL_DATE',
 };
 
-export default function Actions(props) {
+export default function Actions({todo, onUpdate, onGoBack}) {
   const [buttonSet, setButtonSet] = useState(BUTTON_SET.DEFAULT);
 
   switch (buttonSet) {
     case BUTTON_SET.DEFAULT:
       return (
-        <Default {...props} onDefer={() => setButtonSet(BUTTON_SET.DEFER)} />
+        <Default
+          todo={todo}
+          onUpdate={onUpdate}
+          onGoBack={onGoBack}
+          onDefer={() => setButtonSet(BUTTON_SET.DEFER)}
+        />
       );
     case BUTTON_SET.DEFER:
       return (
         <Defer
-          {...props}
+          todo={todo}
+          onUpdate={onUpdate}
           onCancel={() => setButtonSet(BUTTON_SET.DEFAULT)}
           onComplete={() => setButtonSet(BUTTON_SET.DEFAULT)}
         />
