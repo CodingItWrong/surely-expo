ebdbac5c9f99e394d91c1f2f21233317e54a3b31
Josh Justice <me@codingitwrong.com>
2021-09-17 07:12:05 -0400

    Add left and right margin to view screen

--- src/screens/TodoDetail/DetailDisplay/index.js
+++ src/screens/TodoDetail/DetailDisplay/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {ScrollView} from 'react-native';
+import {ScrollView, StyleSheet} from 'react-native';
 import {IconButton, Text, Title} from 'react-native-paper';
 import Actions from './Actions';
 import EventLog from './EventLog';
 
 export default function DetailDisplay({todo, onEdit, onUpdate, onGoBack}) {
   return (
-    <ScrollView>
+    <ScrollView contentContainerStyle={styles.bodyContainer}>
       <IconButton icon="pencil" accessibilityLabel="Edit" onPress={onEdit} />
       <Title>{todo.attributes.name}</Title>
       {todo.attributes.notes ? <Text>{todo.attributes.notes}</Text> : null}
@@ -15,3 +15,9 @@ export default function DetailDisplay({todo, onEdit, onUpdate, onGoBack}) {
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  bodyContainer: {
+    marginHorizontal: 15,
+  },
+});
