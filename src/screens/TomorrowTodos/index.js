7fa7720c07c37096b4750d7661ae6cb7d8dc3b89
Josh Justice <me@codingitwrong.com>
2021-09-18 09:09:46 -0400

    Add category grouping to tomorrow page

--- src/screens/TomorrowTodos/index.js
+++ src/screens/TomorrowTodos/index.js
@@ -1,45 +1,34 @@
 import {useFocusEffect, useLinkTo} from '@react-navigation/native';
 import addDays from 'date-fns/addDays';
 import startOfDay from 'date-fns/startOfDay';
-import filter from 'lodash/filter';
-import sortBy from 'lodash/sortBy';
 import React, {useCallback, useMemo, useRef, useState} from 'react';
-import {FlatList} from 'react-native';
+import {SectionList} from 'react-native';
 import {ActivityIndicator, Button, List, Text} from 'react-native-paper';
 import NewTodoForm from '../../components/NewTodoForm';
 import {useTodos} from '../../data/todos';
+import {groupByCategory} from '../../utils/grouping';
 
 const today = now => startOfDay(new Date());
 const tomorrow = now => addDays(today(now), 1);
-
-const sortedTomorrowTodos = todos =>
-  sortBy(
-    filter(todos, todo => {
-      const deferredUntil = new Date(todo.attributes['deferred-until']);
-      const date = new Date();
-      return (
-        !todo.attributes['deleted-at'] &&
-        !todo.attributes['completed-at'] &&
-        deferredUntil > today(date) &&
-        deferredUntil <= tomorrow(date)
-      );
-    }),
-    [t => t.attributes.name.toLowerCase()],
-  );
+const groupsToSections = groups =>
+  groups.map(({name, todos}) => ({title: name, data: todos}));
 
 export default function AvailableTodos() {
   const todoClient = useTodos();
   const linkTo = useLinkTo();
   const [showLoadingIndicator, setShowLoadingIndicator] = useState(true);
-  const [todos, setTodos] = useState([]);
-  const sortedTodos = useMemo(() => sortedTomorrowTodos(todos), [todos]);
+  const [todoResponse, setTodoResponse] = useState([]);
+  const todoSections = useMemo(
+    () => groupsToSections(groupByCategory(todoResponse)),
+    [todoResponse],
+  );
   const flatListRef = useRef(null);
 
   const loadFromServer = useCallback(
     () =>
       todoClient
-        .where({filter: {status: 'tomorrow'}})
-        .then(({data}) => setTodos(data))
+        .where({filter: {status: 'tomorrow'}, options: {include: 'category'}})
+        .then(response => setTodoResponse(response))
         .then(() => setShowLoadingIndicator(false))
         .catch(console.error),
     [todoClient],
@@ -60,20 +49,25 @@ export default function AvailableTodos() {
   const handleCreate = name =>
     todoClient
       .create({attributes: {name, 'deferred-until': tomorrow(new Date())}})
-      .then(({data}) => setTodos([...todos, data]))
+      .then(loadFromServer)
       .catch(console.error);
 
   function contents() {
     if (showLoadingIndicator) {
       return <ActivityIndicator size="large" />;
-    } else if (sortedTodos.length === 0) {
+    } else if (todoSections.length === 0) {
       return <Text>You have no todos for tomorrow. Nice work!</Text>;
     } else {
       return (
-        <FlatList
+        <SectionList
           ref={flatListRef}
-          data={sortedTodos}
+          sections={todoSections}
           keyExtractor={todo => todo.id}
+          renderSectionHeader={({section}) => (
+            <List.Subheader>
+              {section.title} ({section.data.length})
+            </List.Subheader>
+          )}
           renderItem={({item: todo}) => (
             <List.Item
               key={todo.id}
