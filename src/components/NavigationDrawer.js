7ea64249b92d6aadb5f9f305ab41d31af795dcd4
Josh Justice <me@codingitwrong.com>
2021-09-18 11:58:04 -0400

    Add icons to nav drawer

--- src/components/NavigationDrawer.js
+++ src/components/NavigationDrawer.js
@@ -2,6 +2,14 @@ import {DrawerContentScrollView} from '@react-navigation/drawer';
 import React from 'react';
 import {List} from 'react-native-paper';
 
+const ICON_FOR_ROUTE = {
+  Available: 'clock-outline',
+  Tomorrow: 'weather-night',
+  Future: 'calendar-blank',
+  Completed: 'checkbox-marked',
+  Deleted: 'delete',
+};
+
 export default function CustomNavigationDrawer({logOut, ...navProps}) {
   const {state, navigation} = navProps;
 
@@ -13,6 +21,9 @@ export default function CustomNavigationDrawer({logOut, ...navProps}) {
         <List.Item
           key={route.key}
           title={route.name}
+          left={props => (
+            <List.Icon icon={ICON_FOR_ROUTE[route.name]} {...props} />
+          )}
           onPress={() => navigation.navigate(route.name)}
           style={{
             backgroundColor: isSelected(index) ? '#4caf50' : null,
