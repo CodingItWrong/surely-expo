7ee9f63500f1d697ae674f355f34366d48f8607a
Josh Justice <me@codingitwrong.com>
2021-09-18 07:45:36 -0400

    Add groupByCategory function

---
+++ src/utils/grouping.js
@@ -0,0 +1,26 @@
+import groupBy from 'lodash/groupBy';
+import sortBy from 'lodash/sortBy';
+
+export function groupByCategory(input) {
+  const todos = input.data;
+  const categories = input.included;
+
+  function categoryForTodo(todo) {
+    const categoryId = todo.relationships.category?.data?.id;
+    return categories.find(c => c.id === categoryId);
+  }
+
+  const groupsObject = groupBy(todos, todo => {
+    const category = categoryForTodo(todo);
+    return category?.attributes?.name;
+  });
+  const groups = Object.entries(groupsObject).map(([, groupTodos]) => ({
+    name: categoryForTodo(groupTodos[0])?.attributes?.name ?? 'No Category',
+    todos: sortBy(groupTodos, 'attributes.name'),
+  }));
+  const sortedGroups = sortBy(
+    groups,
+    group => categoryForTodo(group.todos[0])?.sortOrder ?? -9999,
+  );
+  return sortedGroups;
+}
---
+++ src/utils/grouping.spec.js
@@ -0,0 +1,83 @@
+import {groupByCategory} from './grouping';
+
+describe('grouping', () => {
+  describe('groupByCategory', () => {
+    it('creates groups with the category name', () => {
+      const todo1 = {
+        id: 't1',
+        type: 'todos',
+        attributes: {name: 'Todo 1'},
+        relationships: {
+          category: {data: null},
+        },
+      };
+      const todo2 = {
+        id: 't2',
+        type: 'todos',
+        attributes: {name: 'Todo 2'},
+        relationships: {
+          category: {
+            data: {id: 'c1', type: 'categories'},
+          },
+        },
+      };
+      const todo3 = {
+        id: 't3',
+        type: 'todos',
+        attributes: {name: 'Todo 3'},
+        relationships: {
+          category: {
+            data: {id: 'c2', type: 'categories'},
+          },
+        },
+      };
+      const todo4 = {
+        id: 't4',
+        type: 'todos',
+        attributes: {name: 'Todo 4'},
+        relationships: {
+          category: {
+            data: {id: 'c1', type: 'categories'},
+          },
+        },
+      };
+      const category1 = {
+        id: 'c1',
+        type: 'categories',
+        attributes: {
+          name: 'Category 1',
+          'sort-order': 1,
+        },
+      };
+      const category2 = {
+        id: 'c2',
+        type: 'categories',
+        attributes: {
+          name: 'Category 2',
+          'sort-order': 2,
+        },
+      };
+
+      const input = {
+        data: [todo1, todo4, todo3, todo2],
+        included: [category2, category1],
+      };
+      const expectedOutput = [
+        {
+          name: 'No Category',
+          todos: [todo1],
+        },
+        {
+          name: 'Category 1',
+          todos: [todo2, todo4],
+        },
+        {
+          name: 'Category 2',
+          todos: [todo3],
+        },
+      ];
+
+      expect(groupByCategory(input)).toEqual(expectedOutput);
+    });
+  });
+});
