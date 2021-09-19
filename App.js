180189e51988b74ffdce900090a7fa0ad6039f61
Josh Justice <me@codingitwrong.com>
2021-09-19 08:13:13 -0400

    Add category provider

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 import Navigation from './src/Navigation';
 import OAuthLoginContainer from './src/auth/OAuthLoginContainer';
+import {CategoryProvider} from './src/data/categories';
 import {TodoProvider} from './src/data/todos';
 import {TokenProvider} from './src/data/token';
 
@@ -20,7 +21,9 @@ export default function App() {
         <OAuthLoginContainer>
           {({logOut}) => (
             <TodoProvider>
-              <Navigation logOut={logOut} />
+              <CategoryProvider>
+                <Navigation logOut={logOut} />
+              </CategoryProvider>
             </TodoProvider>
           )}
         </OAuthLoginContainer>
