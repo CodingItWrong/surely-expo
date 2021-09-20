cfdf37f463486a266cf62c7094dc656c4f71bbd3
Josh Justice <me@codingitwrong.com>
2021-09-20 06:34:06 -0400

    Add safe area to detail screen

--- src/screens/TodoDetail/DetailDisplay/index.js
+++ src/screens/TodoDetail/DetailDisplay/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {ScrollView, StyleSheet, View} from 'react-native';
 import {IconButton, Text, Title} from 'react-native-paper';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import Actions from './Actions';
 import EventLog from './EventLog';
 
@@ -12,16 +13,22 @@ export default function DetailDisplay({
   onGoBack,
 }) {
   return (
-    <ScrollView contentContainerStyle={styles.bodyContainer}>
-      <View style={styles.editButtonRow}>
-        <IconButton icon="pencil" accessibilityLabel="Edit" onPress={onEdit} />
-      </View>
-      <Title>{todo.attributes.name}</Title>
-      <Text>Category: {category?.attributes?.name ?? 'none'}</Text>
-      {todo.attributes.notes ? <Text>{todo.attributes.notes}</Text> : null}
-      <EventLog todo={todo} />
-      <Actions todo={todo} onUpdate={onUpdate} onGoBack={onGoBack} />
-    </ScrollView>
+    <SafeAreaView edges={['left', 'right', 'bottom']}>
+      <ScrollView contentContainerStyle={styles.bodyContainer}>
+        <View style={styles.editButtonRow}>
+          <IconButton
+            icon="pencil"
+            accessibilityLabel="Edit"
+            onPress={onEdit}
+          />
+        </View>
+        <Title>{todo.attributes.name}</Title>
+        <Text>Category: {category?.attributes?.name ?? 'none'}</Text>
+        {todo.attributes.notes ? <Text>{todo.attributes.notes}</Text> : null}
+        <EventLog todo={todo} />
+        <Actions todo={todo} onUpdate={onUpdate} onGoBack={onGoBack} />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
