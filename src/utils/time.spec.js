f77759e184b0243acd891ecb62517d2186c166bb
Josh Justice <me@codingitwrong.com>
2021-09-26 08:16:13 -0400

    Test relativeDate

--- src/utils/time.spec.js
+++ src/utils/time.spec.js
@@ -1,4 +1,4 @@
-import {relativeDatetime} from './time';
+import {relativeDate, relativeDatetime} from './time';
 
 describe('time utilities', () => {
   describe('relativeDatetime', () => {
@@ -60,6 +60,70 @@ describe('time utilities', () => {
     it('displays seven days from now as full date without time', () => {
       const date = new Date(2021, 0, 17, 13, 14);
       const result = relativeDatetime(date, {now});
+
+      expect(result).toEqual('01/17/2021');
+    });
+  });
+
+  describe('relativeDate', () => {
+    const now = new Date(2021, 0, 10); // Jan 10
+
+    it('returns null if null is passed in', () => {
+      expect(relativeDate(null)).toBeNull();
+    });
+
+    it('displays today as "today"', () => {
+      const date = new Date(2021, 0, 10, 13, 14);
+      const result = relativeDate(date, {now});
+
+      expect(result).toEqual('today');
+    });
+
+    it('handles ISO date strings including time zone adjustment', () => {
+      const dateString = '2021-01-10T18:14:00.000Z';
+      const result = relativeDate(dateString, {now});
+
+      expect(result).toEqual('today');
+    });
+
+    it('displays yesterday as "yesterday"', () => {
+      const date = new Date(2021, 0, 9, 13, 14);
+      const result = relativeDate(date, {now});
+
+      expect(result).toEqual('yesterday');
+    });
+
+    it('displays tomorrow as "tomorrow"', () => {
+      const date = new Date(2021, 0, 11, 13, 14);
+      const result = relativeDate(date, {now});
+
+      expect(result).toEqual('tomorrow');
+    });
+
+    it('displays six days ago as day of week', () => {
+      const date = new Date(2021, 0, 4, 13, 14);
+      const result = relativeDate(date, {now});
+
+      expect(result).toEqual('last Monday');
+    });
+
+    it('displays seven days ago as full date without time', () => {
+      const date = new Date(2021, 0, 3, 13, 14);
+      const result = relativeDate(date, {now});
+
+      expect(result).toEqual('01/03/2021');
+    });
+
+    it('displays six days from now as day of week', () => {
+      const date = new Date(2021, 0, 16, 13, 14);
+      const result = relativeDate(date, {now});
+
+      expect(result).toEqual('Saturday');
+    });
+
+    it('displays seven days from now as full date without time', () => {
+      const date = new Date(2021, 0, 17, 13, 14);
+      const result = relativeDate(date, {now});
 
       expect(result).toEqual('01/17/2021');
     });
