90c5b774ffa9dba1f5bee7eba3550c698eb165f2
Josh Justice <me@codingitwrong.com>
2021-09-28 07:06:12 -0400

    Add test for completed todo

---
+++ cypress/integration/todo-detail-completed.spec.js
@@ -0,0 +1,50 @@
+describe('todo detail - completed', () => {
+  const todoId = 'abc123';
+
+  beforeEach(() => {
+    cy.signIn();
+
+    cy.intercept(
+      'GET',
+      `http://localhost:3000/todos/${todoId}?include=category`,
+      {fixture: 'todo-completed.json'},
+    );
+
+    cy.visit(`/todos/available/${todoId}`);
+  });
+
+  it('displays the todo completion date', () => {
+    cy.contains('Completed 08/28/2021');
+  });
+
+  it('allows uncompleting the todo', () => {
+    cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
+      fixture: 'todo-available.json',
+    }).as('update');
+    cy.intercept('GET', 'http://localhost:3000/todos?*', {});
+
+    cy.getTestId('uncomplete-button').click();
+
+    cy.wait('@update').then(({request}) => {
+      assert.isNull(request.body.data.attributes['completed-at']);
+    });
+
+    cy.url().should('include', '/todos/available');
+  });
+
+  it('allows deleting the todo', () => {
+    // PATCH because it is a soft delete
+    cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
+      fixture: 'todo-available.json',
+    }).as('delete');
+    cy.intercept('GET', 'http://localhost:3000/todos?*', {});
+
+    cy.getTestId('delete-button').click();
+
+    cy.wait('@delete').then(({request}) => {
+      assert.isNotNull(request.body.data.attributes['deleted-at']);
+    });
+
+    cy.url().should('include', '/todos/available');
+  });
+});
