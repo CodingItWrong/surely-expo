30a75c987afc971b0ff83b0a83cbd5366a335677
Josh Justice <me@codingitwrong.com>
2021-09-28 06:57:51 -0400

    Test showing todo detail and completing it

---
+++ cypress/integration/todo-detail.spec.js
@@ -0,0 +1,36 @@
+describe('available todos', () => {
+  const todoId = 'abc123';
+
+  beforeEach(() => {
+    cy.signIn();
+
+    cy.intercept(
+      'GET',
+      `http://localhost:3000/todos/${todoId}?include=category`,
+      {fixture: 'todo-available.json'},
+    );
+
+    cy.visit(`/todos/available/${todoId}`);
+  });
+
+  it('displays the todo content', () => {
+    cy.contains('My Available Todo');
+    cy.contains('Notes for the todo');
+    cy.contains('Created 08/27/2021');
+  });
+
+  it('allows completing the todo', () => {
+    cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
+      fixture: 'todo-available.json',
+    }).as('update');
+    cy.intercept('GET', 'http://localhost:3000/todos?*', {});
+
+    cy.getTestId('complete-button').click();
+
+    cy.wait('@update').then(({request}) => {
+      assert.isNotNull(request.body.data.attributes['completed-at']);
+    });
+
+    cy.url().should('include', '/todos/available');
+  });
+});
