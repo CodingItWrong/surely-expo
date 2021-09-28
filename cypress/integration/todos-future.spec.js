2b4f6ff7dff96d2b0861d2102e442809bde6c440
Josh Justice <me@codingitwrong.com>
2021-09-28 07:58:21 -0400

    Test future list

---
+++ cypress/integration/todos-future.spec.js
@@ -0,0 +1,22 @@
+describe('future todos', () => {
+  beforeEach(() => {
+    cy.signIn();
+
+    cy.intercept(
+      'GET',
+      'http://localhost:3000/todos?filter[status]=future&filter[search]=&sort=name',
+      {fixture: 'todos/future.json'},
+    );
+
+    cy.visit('/todos/future');
+  });
+
+  it('lists existing future todos', () => {
+    cy.getTestId('future-todos').contains('Todo 1');
+  });
+
+  it('allows navigating to a todo detail', () => {
+    cy.getTestId('future-todos').contains('Todo 1').click();
+    cy.url().should('include', '/todos/future/abc123');
+  });
+});
