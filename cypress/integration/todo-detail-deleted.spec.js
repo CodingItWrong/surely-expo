d55d1c09f1d3fdc6d0068b1e4f3576c519f72c95
Josh Justice <me@codingitwrong.com>
2021-09-28 07:09:11 -0400

    Test deleted todos

---
+++ cypress/integration/todo-detail-deleted.spec.js
@@ -0,0 +1,36 @@
+describe('todo detail - completed', () => {
+  const todoId = 'abc123';
+
+  beforeEach(() => {
+    cy.signIn();
+
+    cy.intercept(
+      'GET',
+      `http://localhost:3000/todos/${todoId}?include=category`,
+      {fixture: 'todo-deleted.json'},
+    );
+
+    cy.visit(`/todos/deleted/${todoId}`);
+  });
+
+  it('displays the todo dates', () => {
+    cy.contains('Completed 08/28/2021');
+    cy.contains('Deleted 08/29/2021');
+  });
+
+  it('allows undeleting the todo', () => {
+    cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
+      fixture: 'todo-available.json',
+    }).as('update');
+    cy.intercept('GET', 'http://localhost:3000/todos?*', {});
+
+    cy.getTestId('undelete-button').click();
+
+    cy.wait('@update').then(({request}) => {
+      assert.isNull(request.body.data.attributes['deleted-at']);
+      assert.isNull(request.body.data.attributes['completed-at']);
+    });
+
+    cy.url().should('include', '/todos/deleted');
+  });
+});
