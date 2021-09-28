ce2ad879d32195e9b4897df1915df2ef4c6a500d
Josh Justice <me@codingitwrong.com>
2021-09-28 06:59:13 -0400

    Test deleting the todo

--- cypress/integration/todo-detail.spec.js
+++ cypress/integration/todo-detail.spec.js
@@ -33,4 +33,20 @@ describe('available todos', () => {
 
     cy.url().should('include', '/todos/available');
   });
+
+  it('allows deleting the todo', () => {
+    // PATCH because it is a soft delete
+    cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
+      fixture: 'todo-available.json',
+    }).as('delete');
+    cy.intercept('GET', 'http://localhost:3000/todos?*', {});
+
+    cy.getTestId('delete-button').click();
+
+    cy.wait('@delete').then(({request}) => {
+      assert.isNotNull(request.body.data.attributes['deleted-at']);
+    });
+
+    cy.url().should('include', '/todos/available');
+  });
 });
