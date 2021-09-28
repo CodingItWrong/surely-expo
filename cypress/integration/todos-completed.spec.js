0760820f8a7da9728462a370ada929eb8d3318b8
Josh Justice <me@codingitwrong.com>
2021-09-28 08:39:21 -0400

    Test searching completed and deleted

--- cypress/integration/todos-completed.spec.js
+++ cypress/integration/todos-completed.spec.js
@@ -15,6 +15,19 @@ describe('completed todos', () => {
     cy.getTestId('completed-todos').contains('Todo 1');
   });
 
+  it('allows searching for todos', () => {
+    const searchText = 'MySearchText';
+    cy.intercept(
+      'GET',
+      `http://localhost:3000/todos?filter[status]=completed&filter[search]=${searchText}&sort=-completedAt&page[number]=1`,
+      {fixture: 'todos/completed.json'},
+    ).as('search');
+
+    cy.getTestId('search-field').type(`${searchText}{enter}`);
+
+    cy.wait('@search');
+  });
+
   it('allows navigating to a todo detail', () => {
     cy.getTestId('completed-todos').contains('Todo 1').click();
     cy.url().should('include', '/todos/completed/abc123');
--- cypress/integration/todos-deleted.spec.js
+++ cypress/integration/todos-deleted.spec.js
@@ -15,6 +15,19 @@ describe('deleted todos', () => {
     cy.getTestId('deleted-todos').contains('Todo 1');
   });
 
+  it('allows searching for todos', () => {
+    const searchText = 'MySearchText';
+    cy.intercept(
+      'GET',
+      `http://localhost:3000/todos?filter[status]=deleted&filter[search]=${searchText}&sort=-deletedAt&page[number]=1`,
+      {fixture: 'todos/deleted.json'},
+    ).as('search');
+
+    cy.getTestId('search-field').type(`${searchText}{enter}`);
+
+    cy.wait('@search');
+  });
+
   it('allows navigating to a todo detail', () => {
     cy.getTestId('deleted-todos').contains('Todo 1').click();
     cy.url().should('include', '/todos/deleted/abc123');
