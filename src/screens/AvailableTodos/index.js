754498a34517410b9784ab335b73c214fcb01e36
Josh Justice <me@codingitwrong.com>
2021-09-29 06:12:39 -0400

    Extract general TodoListScreen component

--- src/screens/AvailableTodos/index.js
+++ src/screens/AvailableTodos/index.js
@@ -1,90 +1,30 @@
-import {useFocusEffect, useLinkTo} from '@react-navigation/native';
-import React, {useCallback, useMemo, useRef, useState} from 'react';
-import {StyleSheet} from 'react-native';
-import {Button} from 'react-native-paper';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import LoadingIndicator from '../../components/LoadingIndicator';
-import NewTodoForm from '../../components/NewTodoForm';
-import NoTodosMessage from '../../components/NoTodosMessage';
-import TodoList from '../../components/TodoList';
+import {useLinkTo} from '@react-navigation/native';
+import React, {useCallback} from 'react';
 import {useTodos} from '../../data/todos';
-import {groupByCategory} from '../../utils/grouping';
-import {groupsToSections} from '../../utils/ui';
+import TodoListScreen from '../TodoListScreen';
 
 export default function AvailableTodos() {
   const todoClient = useTodos();
   const linkTo = useLinkTo();
-  const [showLoadingIndicator, setShowLoadingIndicator] = useState(true);
-  const [todoResponse, setTodoResponse] = useState([]);
-  const todoSections = useMemo(
-    () => groupsToSections(groupByCategory(todoResponse)),
-    [todoResponse],
-  );
-  const sectionListRef = useRef(null);
-
-  const loadFromServer = useCallback(
+  const loadAvailableTodos = useCallback(
     () =>
-      todoClient
-        .where({filter: {status: 'available'}, options: {include: 'category'}})
-        .then(response => {
-          setShowLoadingIndicator(false);
-          setTodoResponse(response);
-          return response;
-        })
-        .catch(console.error),
+      todoClient.where({
+        filter: {status: 'available'},
+        options: {include: 'category'},
+      }),
     [todoClient],
   );
 
-  useFocusEffect(
-    useCallback(() => {
-      loadFromServer();
-    }, [loadFromServer]),
-  );
+  const createAvailableTodo = name => todoClient.create({attributes: {name}});
 
-  async function reload() {
-    setShowLoadingIndicator(true);
-    const response = await loadFromServer();
-    if (response.data.length > 0) {
-      sectionListRef.current.scrollToLocation({sectionIndex: 0, itemIndex: 0});
-    }
-  }
-
-  const handleCreate = name =>
-    todoClient
-      .create({attributes: {name}})
-      .then(loadFromServer)
-      .catch(console.error);
-
-  function contents() {
-    if (showLoadingIndicator) {
-      return <LoadingIndicator />;
-    } else if (todoSections.length === 0) {
-      return (
-        <NoTodosMessage>You have no available todos. Nice work!</NoTodosMessage>
-      );
-    } else {
-      return (
-        <TodoList
-          testID="available-todos"
-          sectionListRef={sectionListRef}
-          todoSections={todoSections}
-          onPressTodo={todo => linkTo(`/todos/available/${todo.id}`)}
-        />
-      );
-    }
-  }
+  const goToAvailableTodo = todo => linkTo(`/todos/available/${todo.id}`);
 
   return (
-    <SafeAreaView edges={['left', 'right', 'bottom']} style={styles.safeArea}>
-      <NewTodoForm onCreate={handleCreate} />
-      <Button onPress={reload}>Reload</Button>
-      {contents()}
-    </SafeAreaView>
+    <TodoListScreen
+      onLoadTodos={loadAvailableTodos}
+      onCreateTodo={createAvailableTodo}
+      onPressTodo={goToAvailableTodo}
+      noTodosMessage="You have no available todos. Nice work!"
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-  },
-});
---
+++ src/screens/TodoListScreen.js
@@ -0,0 +1,86 @@
+import {useFocusEffect} from '@react-navigation/native';
+import React, {useCallback, useMemo, useRef, useState} from 'react';
+import {StyleSheet} from 'react-native';
+import {Button} from 'react-native-paper';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import LoadingIndicator from '../components/LoadingIndicator';
+import NewTodoForm from '../components/NewTodoForm';
+import NoTodosMessage from '../components/NoTodosMessage';
+import TodoList from '../components/TodoList';
+import {groupByCategory} from '../utils/grouping';
+import {groupsToSections} from '../utils/ui';
+
+export default function TodoListScreen({
+  onLoadTodos,
+  onCreateTodo,
+  onPressTodo,
+  noTodosMessage,
+}) {
+  const [showLoadingIndicator, setShowLoadingIndicator] = useState(true);
+  const [todoResponse, setTodoResponse] = useState([]);
+  const todoSections = useMemo(
+    () => groupsToSections(groupByCategory(todoResponse)),
+    [todoResponse],
+  );
+  const sectionListRef = useRef(null);
+
+  const loadFromServer = useCallback(
+    () =>
+      onLoadTodos()
+        .then(response => {
+          setShowLoadingIndicator(false);
+          setTodoResponse(response);
+          return response;
+        })
+        .catch(console.error),
+    [onLoadTodos],
+  );
+
+  useFocusEffect(
+    useCallback(() => {
+      loadFromServer();
+    }, [loadFromServer]),
+  );
+
+  const handleCreate = newTodoName =>
+    onCreateTodo(newTodoName).then(loadFromServer).catch(console.error);
+
+  async function reload() {
+    setShowLoadingIndicator(true);
+    const response = await loadFromServer();
+    if (response.data.length > 0) {
+      sectionListRef.current.scrollToLocation({sectionIndex: 0, itemIndex: 0});
+    }
+  }
+
+  function contents() {
+    if (showLoadingIndicator) {
+      return <LoadingIndicator />;
+    } else if (todoSections.length === 0) {
+      return <NoTodosMessage>{noTodosMessage}</NoTodosMessage>;
+    } else {
+      return (
+        <TodoList
+          testID="available-todos"
+          sectionListRef={sectionListRef}
+          todoSections={todoSections}
+          onPressTodo={onPressTodo}
+        />
+      );
+    }
+  }
+
+  return (
+    <SafeAreaView edges={['left', 'right', 'bottom']} style={styles.safeArea}>
+      <NewTodoForm onCreate={handleCreate} />
+      <Button onPress={reload}>Reload</Button>
+      {contents()}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+});
