769b5e54749335925ac2443b9cbe7f1089d47bb5
Josh Justice <me@codingitwrong.com>
2021-10-03 08:44:16 -0400

    Make subheaders opaque

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {SectionList} from 'react-native';
+import {SectionList, StyleSheet} from 'react-native';
 import {List} from 'react-native-paper';
+import theme from '../theme';
 
 export default function TodoList({
   testID,
@@ -21,7 +22,7 @@ export default function TodoList({
       keyExtractor={todo => todo.id}
       contentContainerStyle={contentContainerStyle}
       renderSectionHeader={({section}) => (
-        <List.Subheader>
+        <List.Subheader style={styles.subheader}>
           {section.title} ({section.data.length})
         </List.Subheader>
       )}
@@ -36,3 +37,9 @@ export default function TodoList({
     />
   );
 }
+
+const styles = StyleSheet.create({
+  subheader: {
+    backgroundColor: theme.colors.background,
+  },
+});
