c69e2fb19d29e4ac4d30287f770804c5137e6478
Josh Justice <me@codingitwrong.com>
2021-10-03 14:46:54 -0400

    Add styles and error message to login form

--- src/screens/Login/LoginForm.js
+++ src/screens/Login/LoginForm.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import {View} from 'react-native';
 import {Appbar, Button, Text, TextInput} from 'react-native-paper';
 import useLoginForm from '../../auth/useLoginForm';
+import ErrorMessage from '../../components/ErrorMessage';
+import sharedStyles from '../../sharedStyles';
 
 export default function LoginForm({showSignedUpMessage, onLogIn, onSignUp}) {
   const {username, password, error, handleChange, handleLogIn} =
@@ -11,37 +14,49 @@ export default function LoginForm({showSignedUpMessage, onLogIn, onSignUp}) {
       <Appbar.Header>
         <Appbar.Content title="Surely" />
       </Appbar.Header>
-      {error && <Text>{error}</Text>}
-      {showSignedUpMessage && (
-        <Text>
-          Sign up successful. Log in with the email and password you used to
-          sign up.
-        </Text>
-      )}
-      <TextInput
-        testID="email-field"
-        label="Email"
-        value={username}
-        onChangeText={handleChange('username')}
-        onSubmitEditing={handleLogIn}
-        keyboardType="email-address"
-        autoCapitalize="none"
-        autoCorrect={false}
-      />
-      <TextInput
-        testID="password-field"
-        label="Password"
-        value={password}
-        onChangeText={handleChange('password')}
-        onSubmitEditing={handleLogIn}
-        secureTextEntry
-      />
-      <Button mode="outlined" testID="sign-up-button" onPress={onSignUp}>
-        Sign up
-      </Button>
-      <Button mode="contained" testID="sign-in-button" onPress={handleLogIn}>
-        Sign in
-      </Button>
+      <View style={sharedStyles.bodyContainer}>
+        {showSignedUpMessage && (
+          <Text>
+            Sign up successful. Log in with the email and password you used to
+            sign up.
+          </Text>
+        )}
+        <TextInput
+          testID="email-field"
+          label="Email"
+          value={username}
+          onChangeText={handleChange('username')}
+          onSubmitEditing={handleLogIn}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <TextInput
+          testID="password-field"
+          label="Password"
+          value={password}
+          onChangeText={handleChange('password')}
+          onSubmitEditing={handleLogIn}
+          secureTextEntry
+        />
+        <ErrorMessage>{error}</ErrorMessage>
+        <Button
+          mode="outlined"
+          testID="sign-up-button"
+          onPress={onSignUp}
+          style={sharedStyles.buttonSpacing}
+        >
+          Sign up
+        </Button>
+        <Button
+          mode="contained"
+          testID="sign-in-button"
+          onPress={handleLogIn}
+          style={sharedStyles.buttonSpacing}
+        >
+          Sign in
+        </Button>
+      </View>
     </>
   );
 }
