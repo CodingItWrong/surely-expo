6a413b81035716712da7de37d1b2358dd166e475
Josh Justice <me@codingitwrong.com>
2021-10-03 13:50:47 -0400

    Add reload button

--- src/screens/CategoryList.js
+++ src/screens/CategoryList.js
@@ -1,7 +1,7 @@
 import {useLinkTo} from '@react-navigation/native';
 import sortBy from 'lodash/sortBy';
-import React, {useCallback, useEffect, useState} from 'react';
-import {FlatList, StyleSheet, View} from 'react-native';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import {FlatList, Platform, StyleSheet, View} from 'react-native';
 import {Button, IconButton, List} from 'react-native-paper';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import LoadingIndicator from '../components/LoadingIndicator';
@@ -15,6 +15,7 @@ import {
 
 export default function CategoryList() {
   const insets = useSafeAreaInsets();
+  const flatListRef = useRef();
   const [showLoadingIndicator, setShowLoadingIndicator] = useState(true);
   const [categories, setCategories] = useState(null);
   const categoryClient = useCategories();
@@ -24,21 +25,22 @@ export default function CategoryList() {
 
   const handleAdd = () => linkTo('/categories/new');
 
-  const loadCategories = useCallback(
+  const loadFromServer = useCallback(
     () =>
       categoryClient
         .all()
         .then(({data}) => {
           setCategories(sortBy(data, 'attributes.sort-order'));
           setShowLoadingIndicator(false);
+          return data;
         })
         .catch(console.error),
     [categoryClient],
   );
 
   useEffect(() => {
-    loadCategories();
-  }, [loadCategories]);
+    loadFromServer();
+  }, [loadFromServer]);
 
   function moveUpward(categoryToMove) {
     const categoriesAfterMove = arrayWithItemMovedUpward(
@@ -66,58 +68,76 @@ export default function CategoryList() {
         });
       }
     }
-    return loadCategories();
+    return loadFromServer();
   }
 
-  if (showLoadingIndicator) {
-    return <LoadingIndicator />;
-  } else if (categories.length === 0) {
-    return <NoTodosMessage>No categories yet</NoTodosMessage>;
-  } else {
-    return (
-      <View style={styles.listContainer}>
-        <Button
-          testID="add-button"
-          mode="outlined"
-          icon="plus"
-          onPress={handleAdd}
-        >
-          Add
-        </Button>
-        <FlatList
-          testID="category-list"
-          data={categories}
-          keyExtractor={todo => todo.id}
-          contentContainerStyle={{paddingBottom: insets.bottom}}
-          renderItem={({item: category}) => (
-            <List.Item
-              key={category.id}
-              title={category.attributes.name}
-              onPress={() => onPressCategory(category)}
-              right={() => (
-                <>
-                  <IconButton
-                    testID="move-up-button"
-                    icon="arrow-up"
-                    onPress={() => moveUpward(category)}
-                  />
-                  <IconButton
-                    testID="move-down-button"
-                    icon="arrow-down"
-                    onPress={() => moveDownward(category)}
-                  />
-                </>
-              )}
-            />
-          )}
-        />
-      </View>
-    );
+  async function reloadFromButton() {
+    setShowLoadingIndicator(true);
+    const newCategories = await loadFromServer();
+    if (newCategories.length > 0) {
+      flatListRef.current.scrollToIndex({index: 0});
+    }
   }
+
+  function contents() {
+    if (showLoadingIndicator) {
+      return <LoadingIndicator />;
+    } else if (categories.length === 0) {
+      return <NoTodosMessage>No categories yet</NoTodosMessage>;
+    } else {
+      return (
+        <View style={styles.listContainer}>
+          <Button
+            testID="add-button"
+            mode="outlined"
+            icon="plus"
+            onPress={handleAdd}
+          >
+            Add
+          </Button>
+          <FlatList
+            testID="category-list"
+            ref={flatListRef}
+            data={categories}
+            keyExtractor={todo => todo.id}
+            contentContainerStyle={{paddingBottom: insets.bottom}}
+            renderItem={({item: category}) => (
+              <List.Item
+                key={category.id}
+                title={category.attributes.name}
+                onPress={() => onPressCategory(category)}
+                right={() => (
+                  <>
+                    <IconButton
+                      testID="move-up-button"
+                      icon="arrow-up"
+                      onPress={() => moveUpward(category)}
+                    />
+                    <IconButton
+                      testID="move-down-button"
+                      icon="arrow-down"
+                      onPress={() => moveDownward(category)}
+                    />
+                  </>
+                )}
+              />
+            )}
+          />
+        </View>
+      );
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button onPress={reloadFromButton}>Reload</Button>
+      {contents()}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  listContainer: {
+  container: {
     flex: 1,
   },
 });
