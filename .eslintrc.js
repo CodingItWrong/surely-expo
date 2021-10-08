154c486d7c957df61b9c4ca3508a7d986418323a
Josh Justice <me@codingitwrong.com>
2021-10-08 06:42:50 -0400

    Update JSX renderer to remove need for React import

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
     'import/order': ['warn', {alphabetize: {order: 'asc'}}], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
     'no-duplicate-imports': 'error',
     quotes: ['error', 'single', {avoidEscape: true}], // single quote unless using interpolation
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
     'sort-imports': [
       'warn',
       {ignoreDeclarationSort: true, ignoreMemberSort: false},
