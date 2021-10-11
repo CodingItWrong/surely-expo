f8380611d47e4ff099abd3ba1c43a747d572d935
Josh Justice <me@codingitwrong.com>
2021-10-11 07:00:51 -0400

    Center defer buttons on large

--- src/screens/TodoDetail/DetailDisplay/Actions/Defer.js
+++ src/screens/TodoDetail/DetailDisplay/Actions/Defer.js
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import {Button} from 'react-native-paper';
 import {DatePickerModal} from 'react-native-paper-dates';
 import ErrorMessage from '../../../../components/ErrorMessage';
+import VerticalButtonGroup from '../../../../components/VerticalButtonGroup';
 import {useTodos} from '../../../../data/todos';
 import sharedStyles from '../../../../sharedStyles';
 import {dayOfWeek, deferDate} from '../../../../utils/time';
@@ -38,55 +39,57 @@ export default function Defer({todo, onUpdate, onCancel, onComplete}) {
   return (
     <>
       <ErrorMessage>{errorMessage}</ErrorMessage>
-      <Button
-        testID="cancel-defer-button"
-        mode="outlined"
-        onPress={onCancel}
-        style={sharedStyles.buttonSpacing}
-        disabled={isLoading}
-      >
-        Cancel
-      </Button>
-      <DeferButton
-        testID="defer-1-day-button"
-        todo={todo}
-        numDays={1}
-        label="1 Day"
-        onDefer={handleDeferUntil}
-        disabled={isLoading}
-      />
-      <DeferButton
-        testID="defer-2-days-button"
-        todo={todo}
-        numDays={2}
-        label="2 Days"
-        onDefer={handleDeferUntil}
-        disabled={isLoading}
-      />
-      <DeferButton
-        testID="defer-3-days-button"
-        todo={todo}
-        numDays={3}
-        label="3 Days"
-        onDefer={handleDeferUntil}
-        disabled={isLoading}
-      />
-      <DeferButton
-        testID="defer-1-week-button"
-        todo={todo}
-        numDays={7}
-        label="1 Week"
-        onDefer={handleDeferUntil}
-        disabled={isLoading}
-      />
-      <Button
-        mode="outlined"
-        onPress={() => setIsDeferredUntilModalOpen(true)}
-        style={sharedStyles.buttonSpacing}
-        disabled={isLoading}
-      >
-        Pick Date
-      </Button>
+      <VerticalButtonGroup>
+        <Button
+          testID="cancel-defer-button"
+          mode="outlined"
+          onPress={onCancel}
+          style={sharedStyles.buttonSpacing}
+          disabled={isLoading}
+        >
+          Cancel
+        </Button>
+        <DeferButton
+          testID="defer-1-day-button"
+          todo={todo}
+          numDays={1}
+          label="1 Day"
+          onDefer={handleDeferUntil}
+          disabled={isLoading}
+        />
+        <DeferButton
+          testID="defer-2-days-button"
+          todo={todo}
+          numDays={2}
+          label="2 Days"
+          onDefer={handleDeferUntil}
+          disabled={isLoading}
+        />
+        <DeferButton
+          testID="defer-3-days-button"
+          todo={todo}
+          numDays={3}
+          label="3 Days"
+          onDefer={handleDeferUntil}
+          disabled={isLoading}
+        />
+        <DeferButton
+          testID="defer-1-week-button"
+          todo={todo}
+          numDays={7}
+          label="1 Week"
+          onDefer={handleDeferUntil}
+          disabled={isLoading}
+        />
+        <Button
+          mode="outlined"
+          onPress={() => setIsDeferredUntilModalOpen(true)}
+          style={sharedStyles.buttonSpacing}
+          disabled={isLoading}
+        >
+          Pick Date
+        </Button>
+      </VerticalButtonGroup>
       <DatePickerModal
         visible={isDeferredUntilModalOpen}
         mode="single"
