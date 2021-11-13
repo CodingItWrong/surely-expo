f969de11d92524b35b582389776bd50031a4aaa5
Josh Justice <me@codingitwrong.com>
2021-11-13 07:50:29 -0500

    Format fix

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import {Provider as ForkPaperProvider} from '@codingitwrong/react-native-paper';
-import {Provider as PaperProvider} from 'react-native-paper';
 import {en, registerTranslation} from '@codingitwrong/react-native-paper-dates';
 import {StatusBar} from 'expo-status-bar';
 // for some reason PaperProvider errors without the React import
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import {Provider as PaperProvider} from 'react-native-paper';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Navigation from './src/Navigation';
 import TokenLoadBuffer from './src/components/TokenLoadBuffer';
@@ -19,16 +19,16 @@ export default function App() {
   const theme = useTheme();
   return (
     <ForkPaperProvider theme={theme}>
-    <PaperProvider theme={theme}>
-      <StatusBar style="auto" />
-      <SafeAreaProvider>
-        <TokenProvider>
-          <TokenLoadBuffer>
-            <Navigation />
-          </TokenLoadBuffer>
-        </TokenProvider>
-      </SafeAreaProvider>
-    </PaperProvider>
+      <PaperProvider theme={theme}>
+        <StatusBar style="auto" />
+        <SafeAreaProvider>
+          <TokenProvider>
+            <TokenLoadBuffer>
+              <Navigation />
+            </TokenLoadBuffer>
+          </TokenProvider>
+        </SafeAreaProvider>
+      </PaperProvider>
     </ForkPaperProvider>
   );
 }
