fa7d0415a9f3e46349588d42ebfd6c7678b64a4f
Josh Justice <me@codingitwrong.com>
2021-11-24 07:41:00 -0500

    Resort completed list specs

--- cypress/integration/todos-completed.spec.js
+++ cypress/integration/todos-completed.spec.js
@@ -30,6 +30,28 @@ describe('completed todos', () => {
     cy.contains("You have no completed todos. You'll get there!");
   });
 
+  it('allows navigating to a todo detail', () => {
+    cy.getTestId('todo-list').contains('Todo 1').click();
+    cy.url().should('include', '/todos/completed/abc123');
+  });
+
+  it('allows page navigation', () => {
+    // wait for page to load
+    cy.getTestId('todo-list').contains('Todo 1');
+
+    cy.intercept(
+      'GET',
+      'http://localhost:3000/todos?filter[status]=completed&filter[search]=&sort=-completedAt&page[number]=2',
+      {fixture: 'todos/completed-page2.json'},
+    );
+
+    cy.getTestId('next-page-button').click();
+    cy.getTestId('todo-list').contains('Todo 4');
+
+    cy.getTestId('previous-page-button').click();
+    cy.getTestId('todo-list').contains('Todo 1');
+  });
+
   it('allows searching for todos', () => {
     cy.intercept(
       'GET',
@@ -52,27 +74,5 @@ describe('completed todos', () => {
     cy.getTestId('search-field').type(`${searchText}{enter}`);
 
     cy.contains('No completed todos matched your search');
-  });
-
-  it('allows navigating to a todo detail', () => {
-    cy.getTestId('todo-list').contains('Todo 1').click();
-    cy.url().should('include', '/todos/completed/abc123');
-  });
-
-  it('allows page navigation', () => {
-    // wait for page to load
-    cy.getTestId('todo-list').contains('Todo 1');
-
-    cy.intercept(
-      'GET',
-      'http://localhost:3000/todos?filter[status]=completed&filter[search]=&sort=-completedAt&page[number]=2',
-      {fixture: 'todos/completed-page2.json'},
-    );
-
-    cy.getTestId('next-page-button').click();
-    cy.getTestId('todo-list').contains('Todo 4');
-
-    cy.getTestId('previous-page-button').click();
-    cy.getTestId('todo-list').contains('Todo 1');
   });
 });
