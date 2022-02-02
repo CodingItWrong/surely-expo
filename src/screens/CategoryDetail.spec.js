ab959ef995a09d8a9b5e9c50f01bd4ecb4b22715
Josh Justice <me@codingitwrong.com>
2022-02-02 07:55:13 -0500

    Component test for creating category

---
+++ src/screens/CategoryDetail.spec.js
@@ -0,0 +1,69 @@
+import {useLinkTo} from '@react-navigation/native';
+import {fireEvent, render, waitFor} from '@testing-library/react-native';
+import authenticatedHttpClient from '../data/authenticatedHttpClient';
+import {TokenProvider} from '../data/token';
+import CategoryDetail from './CategoryDetail';
+
+jest.mock('../data/authenticatedHttpClient', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+jest.mock('@react-navigation/native', () => ({
+  useLinkTo: jest.fn(),
+}));
+
+describe('CategoryDetail', () => {
+  it('allows creating the category', async () => {
+    const name = 'New Category';
+    const linkTo = jest.fn();
+    useLinkTo.mockReturnValue(linkTo);
+
+    const request = [
+      'categories?',
+      {data: {attributes: {name: 'New Category'}, type: 'categories'}},
+      {headers: {'Content-Type': 'application/vnd.api+json'}},
+    ];
+    const response = {
+      data: {
+        id: 'abc123',
+        type: 'todos',
+        attributes: {
+          name: 'My Available Todo',
+          notes: 'Notes for the todo',
+          'created-at': '2021-08-27T23:54:49.483Z',
+          'updated-at': '2021-08-27T23:54:49.483Z',
+          'deleted-at': null,
+          'completed-at': null,
+          'deferred-at': null,
+          'deferred-until': null,
+        },
+        relationships: {
+          category: {
+            data: null,
+          },
+        },
+      },
+    };
+
+    const client = {
+      post: jest.fn().mockResolvedValue(response),
+    };
+    authenticatedHttpClient.mockReturnValue(client);
+
+    const route = {params: {id: 'new'}};
+    const {getByTestId} = render(
+      <TokenProvider loadToken={false}>
+        <CategoryDetail route={route} />
+      </TokenProvider>,
+    );
+
+    fireEvent.changeText(getByTestId('name-field'), name);
+    fireEvent.press(getByTestId('save-button'));
+
+    await waitFor(() => {
+      expect(client.post).toHaveBeenCalledWith(...request);
+      expect(linkTo).toHaveBeenCalledWith('/categories');
+    });
+  });
+});
