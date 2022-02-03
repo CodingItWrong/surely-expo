e2fff3411dab478afa7fe9ea07ef495f800a1811
Josh Justice <me@codingitwrong.com>
2022-02-03 07:28:05 -0500

    Handle clearing load error in component test

--- src/screens/TodoDetail/index.spec.js
+++ src/screens/TodoDetail/index.spec.js
@@ -1,4 +1,4 @@
-import {render, waitFor} from '@testing-library/react-native';
+import {fireEvent, render, waitFor} from '@testing-library/react-native';
 import authenticatedHttpClient from '../../data/authenticatedHttpClient';
 import {TokenProvider} from '../../data/token';
 import {createTodoDetail} from './index';
@@ -33,6 +33,58 @@ describe('TodoDetail', () => {
         expect(client.get).toHaveBeenCalledWith(
           `todos/${todoId}?include=category`,
         );
+      });
+    });
+
+    it('clears the error upon successful retry', async () => {
+      const client = {
+        get: jest.fn().mockRejectedValue(),
+      };
+      authenticatedHttpClient.mockReturnValue(client);
+
+      const todoId = '42';
+      const route = {params: {id: todoId}};
+      const {getByTestId, queryByText} = render(
+        <TokenProvider loadToken={false}>
+          <AvailableTodoDetail route={route} />
+        </TokenProvider>,
+      );
+
+      await waitFor(() =>
+        expect(
+          queryByText('An error occurred loading the todo.'),
+        ).not.toBeNull(),
+      );
+
+      client.get.mockResolvedValue({
+        data: {
+          data: {
+            id: 'abc123',
+            type: 'todos',
+            attributes: {
+              name: 'My Available Todo',
+              notes: 'Notes for the todo',
+              'created-at': '2021-08-27T23:54:49.483Z',
+              'updated-at': '2021-08-27T23:54:49.483Z',
+              'deleted-at': null,
+              'completed-at': null,
+              'deferred-at': null,
+              'deferred-until': null,
+            },
+            relationships: {
+              category: {
+                data: null,
+              },
+            },
+          },
+        },
+      });
+
+      fireEvent.press(getByTestId('retry-button'));
+
+      await waitFor(() => {
+        expect(queryByText('My Available Todo')).not.toBeNull();
+        expect(queryByText('An error occurred loading the todo.')).toBeNull();
       });
     });
   });
