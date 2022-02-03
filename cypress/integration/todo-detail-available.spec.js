314d0e59d67f6bef31ce66a1af4de778c2409c26
Josh Justice <me@codingitwrong.com>
2022-02-03 08:47:24 -0500

    Fix flaky test for todo load failure

--- cypress/integration/todo-detail-available.spec.js
+++ cypress/integration/todo-detail-available.spec.js
@@ -1,172 +1,183 @@
 describe('todo detail - available', () => {
   const todoId = 'abc123';
 
-  beforeEach(() => {
-    cy.signIn();
+  describe('when there is an error loading the todo', () => {
+    beforeEach(() => {
+      cy.signIn();
 
-    cy.intercept(
-      'GET',
-      `http://localhost:3000/todos/${todoId}?include=category`,
-      {fixture: 'todo/available.json'},
-    );
+      cy.intercept(
+        'GET',
+        `http://localhost:3000/todos/${todoId}?include=category`,
+        {statusCode: 500},
+      );
 
-    cy.visit(`/todos/available/${todoId}`);
-  });
-
-  it('displays the todo content', () => {
-    cy.contains('My Available Todo');
-    cy.contains('Notes for the todo');
-    cy.contains('Created 08/27/2021');
-  });
-
-  it('shows a message when there is an error loading the todo', () => {
-    cy.intercept(
-      'GET',
-      `http://localhost:3000/todos/${todoId}?include=category`,
-      {statusCode: 500},
-    );
-    cy.contains('An error occurred');
-
-    cy.intercept(
-      'GET',
-      `http://localhost:3000/todos/${todoId}?include=category`,
-      {fixture: 'todo/available.json'},
-    );
-
-    cy.getTestId('retry-button').click();
-
-    cy.contains('My Available Todo');
-  });
-
-  it('allows going back to available todos', () => {
-    cy.getTestId('back-button').click();
-    cy.url().should('match', /\/todos\/available$/);
-  });
-
-  it('allows completing the todo', () => {
-    cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
-      fixture: 'todo/available.json',
-    }).as('update');
-    cy.intercept('GET', 'http://localhost:3000/todos?*', {});
-
-    cy.getTestId('complete-button').click();
-
-    cy.wait('@update').then(({request}) => {
-      assert.isNotNull(request.body.data.attributes['completed-at']);
+      cy.visit(`/todos/available/${todoId}`);
     });
 
-    cy.url().should('include', '/todos/available');
+    it('shows an error message', () => {
+      cy.contains('An error occurred');
+
+      cy.intercept(
+        'GET',
+        `http://localhost:3000/todos/${todoId}?include=category`,
+        {fixture: 'todo/available.json'},
+      );
+
+      cy.getTestId('retry-button').click();
+
+      cy.contains('My Available Todo');
+    });
   });
 
-  it('shows a message when there is an error completing the todo', () => {
-    cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
-      statusCode: 500,
-    });
-    cy.getTestId('complete-button').click();
-    cy.contains('An error occurred');
-  });
+  describe('when the todo loads successfully', () => {
+    beforeEach(() => {
+      cy.signIn();
 
-  it('allows deleting the todo', () => {
-    // PATCH because it is a soft delete
-    cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
-      fixture: 'todo/available.json',
-    }).as('delete');
-    cy.intercept('GET', 'http://localhost:3000/todos?*', {});
+      cy.intercept(
+        'GET',
+        `http://localhost:3000/todos/${todoId}?include=category`,
+        {fixture: 'todo/available.json'},
+      );
 
-    cy.getTestId('delete-button').click();
-
-    cy.wait('@delete').then(({request}) => {
-      assert.isNotNull(request.body.data.attributes['deleted-at']);
+      cy.visit(`/todos/available/${todoId}`);
     });
 
-    cy.url().should('include', '/todos/available');
-  });
-
-  it('shows a message when there is an error deleting the todo', () => {
-    cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
-      statusCode: 500,
+    it('displays the todo content', () => {
+      cy.contains('My Available Todo');
+      cy.contains('Notes for the todo');
+      cy.contains('Created 08/27/2021');
     });
-    cy.getTestId('delete-button').click();
-    cy.contains('An error occurred');
-  });
 
-  it('allows editing the todo', () => {
-    cy.intercept('GET', 'http://localhost:3000/categories?', {
-      fixture: 'categories.json',
+    it('allows going back to available todos', () => {
+      cy.getTestId('back-button').click();
+      cy.url().should('match', /\/todos\/available$/);
     });
-    cy.intercept(
-      'PATCH',
-      `http://localhost:3000/todos/${todoId}?include=category`,
-      {
+
+    it('allows completing the todo', () => {
+      cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
         fixture: 'todo/available.json',
-      },
-    ).as('update');
+      }).as('update');
+      cy.intercept('GET', 'http://localhost:3000/todos?*', {});
 
-    const name = 'Updated Name';
-    const notes = 'Updated Notes';
+      cy.getTestId('complete-button').click();
 
-    cy.getTestId('edit-button').click();
+      cy.wait('@update').then(({request}) => {
+        assert.isNotNull(request.body.data.attributes['completed-at']);
+      });
 
-    cy.getTestId('name-field').clear().type(name);
-    cy.getTestId('notes-field').clear().type(notes);
-    cy.getTestId('cancel-button').click();
-    // cy.should('not.contain', name);
-    // cy.should('not.contain', notes);
-
-    cy.getTestId('edit-button').click();
-
-    cy.getTestId('name-field').clear().type(name);
-    cy.getTestId('notes-field').clear().type(notes);
-    cy.getTestId('choose-category-field').click();
-    cy.contains('Category B').click({force: true});
-    cy.getTestId('save-button').click();
-
-    cy.wait('@update').then(({request}) => {
-      const {attributes, relationships} = request.body.data;
-      assert.equal(attributes.name, name);
-      assert.equal(attributes.notes, notes);
-      assert.equal(relationships.category.data.id, 'cat2');
+      cy.url().should('include', '/todos/available');
     });
 
-    cy.getTestId('edit-button').should('exist');
-  });
-
-  it('shows a message when there is an error saving edits to the todo', () => {
-    cy.intercept('GET', 'http://localhost:3000/categories?', {
-      fixture: 'categories.json',
-    });
-    cy.intercept(
-      'PATCH',
-      `http://localhost:3000/todos/${todoId}?include=category`,
-      {statusCode: 500},
-    );
-    cy.getTestId('edit-button').click();
-    cy.getTestId('save-button').click();
-    cy.contains('An error occurred');
-  });
-
-  it('allows deferring the todo', () => {
-    cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
-      fixture: 'todo/available.json',
-    }).as('update');
-
-    cy.getTestId('defer-button').click();
-    cy.getTestId('defer-1-day-button').click();
-
-    cy.wait('@update').then(({request}) => {
-      assert.isNotNull(request.body.data.attributes['deferred-until']);
+    it('shows a message when there is an error completing the todo', () => {
+      cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
+        statusCode: 500,
+      });
+      cy.getTestId('complete-button').click();
+      cy.contains('An error occurred');
     });
 
-    cy.url().should('include', '/todos/available');
-  });
+    it('allows deleting the todo', () => {
+      // PATCH because it is a soft delete
+      cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
+        fixture: 'todo/available.json',
+      }).as('delete');
+      cy.intercept('GET', 'http://localhost:3000/todos?*', {});
 
-  it('shows a message when an error occurs deferring the todo', () => {
-    cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
-      statusCode: 500,
+      cy.getTestId('delete-button').click();
+
+      cy.wait('@delete').then(({request}) => {
+        assert.isNotNull(request.body.data.attributes['deleted-at']);
+      });
+
+      cy.url().should('include', '/todos/available');
     });
 
-    cy.getTestId('defer-button').click();
-    cy.getTestId('defer-1-day-button').click();
-    cy.contains('An error occurred');
+    it('shows a message when there is an error deleting the todo', () => {
+      cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
+        statusCode: 500,
+      });
+      cy.getTestId('delete-button').click();
+      cy.contains('An error occurred');
+    });
+
+    it('allows editing the todo', () => {
+      cy.intercept('GET', 'http://localhost:3000/categories?', {
+        fixture: 'categories.json',
+      });
+      cy.intercept(
+        'PATCH',
+        `http://localhost:3000/todos/${todoId}?include=category`,
+        {
+          fixture: 'todo/available.json',
+        },
+      ).as('update');
+
+      const name = 'Updated Name';
+      const notes = 'Updated Notes';
+
+      cy.getTestId('edit-button').click();
+
+      cy.getTestId('name-field').clear().type(name);
+      cy.getTestId('notes-field').clear().type(notes);
+      cy.getTestId('cancel-button').click();
+      // cy.should('not.contain', name);
+      // cy.should('not.contain', notes);
+
+      cy.getTestId('edit-button').click();
+
+      cy.getTestId('name-field').clear().type(name);
+      cy.getTestId('notes-field').clear().type(notes);
+      cy.getTestId('choose-category-field').click();
+      cy.contains('Category B').click({force: true});
+      cy.getTestId('save-button').click();
+
+      cy.wait('@update').then(({request}) => {
+        const {attributes, relationships} = request.body.data;
+        assert.equal(attributes.name, name);
+        assert.equal(attributes.notes, notes);
+        assert.equal(relationships.category.data.id, 'cat2');
+      });
+
+      cy.getTestId('edit-button').should('exist');
+    });
+
+    it('shows a message when there is an error saving edits to the todo', () => {
+      cy.intercept('GET', 'http://localhost:3000/categories?', {
+        fixture: 'categories.json',
+      });
+      cy.intercept(
+        'PATCH',
+        `http://localhost:3000/todos/${todoId}?include=category`,
+        {statusCode: 500},
+      );
+      cy.getTestId('edit-button').click();
+      cy.getTestId('save-button').click();
+      cy.contains('An error occurred');
+    });
+
+    it('allows deferring the todo', () => {
+      cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
+        fixture: 'todo/available.json',
+      }).as('update');
+
+      cy.getTestId('defer-button').click();
+      cy.getTestId('defer-1-day-button').click();
+
+      cy.wait('@update').then(({request}) => {
+        assert.isNotNull(request.body.data.attributes['deferred-until']);
+      });
+
+      cy.url().should('include', '/todos/available');
+    });
+
+    it('shows a message when an error occurs deferring the todo', () => {
+      cy.intercept('PATCH', `http://localhost:3000/todos/${todoId}?`, {
+        statusCode: 500,
+      });
+
+      cy.getTestId('defer-button').click();
+      cy.getTestId('defer-1-day-button').click();
+      cy.contains('An error occurred');
+    });
   });
 });
