06ac2eb47935c01d27380bcf32d62c12c3de0ab0
Josh Justice <me@codingitwrong.com>
2022-05-07 14:00:13 -0400

    Move deleted list test to RNTL

---
+++ src/screens/TodoList/Deleted.spec.js
@@ -0,0 +1,55 @@
+import {render} from '@testing-library/react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import authenticatedHttpClient from '../../data/authenticatedHttpClient';
+import {TokenProvider} from '../../data/token';
+import {mockUseFocusEffect, safeAreaMetrics} from '../../testUtils';
+import Deleted from './Deleted';
+
+jest.mock('../../data/authenticatedHttpClient', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+jest.mock('@react-navigation/native', () => ({
+  useFocusEffect: jest.fn(),
+  useLinkTo: jest.fn(),
+}));
+
+describe('Deleted', () => {
+  const todo = {
+    id: 'abc123',
+    type: 'todos',
+    attributes: {
+      name: 'Todo 1',
+      'deleted-at': '2021-08-28T23:54:49.483Z',
+    },
+  };
+
+  beforeEach(() => {
+    mockUseFocusEffect();
+  });
+
+  it('displays completed todos from the server', async () => {
+    const response = {data: [todo]};
+
+    const client = {
+      get: jest.fn().mockResolvedValue({data: response}),
+    };
+    authenticatedHttpClient.mockReturnValue(client);
+
+    const {findByText, queryByText} = render(
+      <SafeAreaProvider initialMetrics={safeAreaMetrics}>
+        <TokenProvider loadToken={false}>
+          <Deleted />
+        </TokenProvider>
+      </SafeAreaProvider>,
+    );
+
+    await findByText(todo.attributes.name);
+    expect(queryByText('08/28/2021 (1)')).not.toBeNull();
+
+    expect(client.get).toHaveBeenCalledWith(
+      'todos?filter[status]=deleted&filter[search]=&sort=-deletedAt&page[number]=1',
+    );
+  });
+});
