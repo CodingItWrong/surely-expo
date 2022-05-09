7aa4e10c96799281b1b116f4ea713fee88a7e460
Josh Justice <me@codingitwrong.com>
2022-05-09 08:41:39 -0400

    Move completed search test to RNTL

--- cypress/integration/todos-completed.spec.js
+++ cypress/integration/todos-completed.spec.js
@@ -30,18 +30,6 @@ describe('completed todos', () => {
     cy.getTestId('todo-list').contains('Todo 1');
   });
 
-  it('allows searching for todos', () => {
-    cy.intercept(
-      'GET',
-      `http://localhost:3000/todos?filter[status]=completed&filter[search]=${searchText}&sort=-completedAt&page[number]=1`,
-      {fixture: 'todos/completed.json'},
-    ).as('search');
-
-    cy.getTestId('search-field').type(`${searchText}{enter}`);
-
-    cy.wait('@search');
-  });
-
   it('resets pagination upon search', () => {
     cy.intercept(
       'GET',
--- src/screens/TodoList/Completed.spec.js
+++ src/screens/TodoList/Completed.spec.js
@@ -1,5 +1,5 @@
 import {useLinkTo} from '@react-navigation/native';
-import {fireEvent, render} from '@testing-library/react-native';
+import {fireEvent, render, waitFor} from '@testing-library/react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import authenticatedHttpClient from '../../data/authenticatedHttpClient';
 import {TokenProvider} from '../../data/token';
@@ -60,7 +60,13 @@ describe('Completed', () => {
       };
       authenticatedHttpClient.mockReturnValue(client);
 
-      const {findByText, getByText, queryByText} = render(
+      const {
+        findByText,
+        getByLabelText,
+        getByText,
+        queryByLabelText,
+        queryByText,
+      } = render(
         <SafeAreaProvider initialMetrics={safeAreaMetrics}>
           <TokenProvider loadToken={false}>
             <Completed />
@@ -68,7 +74,14 @@ describe('Completed', () => {
         </SafeAreaProvider>,
       );
 
-      return {client, findByText, getByText, queryByText};
+      return {
+        client,
+        findByText,
+        getByLabelText,
+        getByText,
+        queryByLabelText,
+        queryByText,
+      };
     }
 
     it('displays completed todos from the server', async () => {
@@ -92,6 +105,24 @@ describe('Completed', () => {
       fireEvent.press(getByText('Todo 1'));
 
       expect(linkTo).toHaveBeenCalledWith('/todos/completed/abc123');
+    });
+
+    it('allows searching for todos', async () => {
+      const searchText = 'MySearchText';
+      const {client, findByText, getByLabelText, queryByLabelText} =
+        renderComponent();
+
+      await findByText('Todo 1');
+
+      const searchField = getByLabelText('search');
+      fireEvent.changeText(searchField, searchText);
+      fireEvent(searchField, 'submitEditing');
+
+      expect(client.get).toHaveBeenLastCalledWith(
+        `todos?filter[status]=completed&filter[search]=${searchText}&sort=-completedAt&page[number]=1`,
+      );
+
+      await waitFor(() => expect(queryByLabelText('Loading')).toBeNull());
     });
   });
 });
