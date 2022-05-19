e169d62ab6f55cb1ccf264bf6f00a0da5c18c026
Josh Justice <me@codingitwrong.com>
2022-05-19 07:11:24 -0400

    Move sign out test to RNTL

---
+++ src/components/NavigationDrawer.spec.js
@@ -0,0 +1,39 @@
+import {fireEvent, render, waitFor} from '@testing-library/react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {useToken} from '../data/token';
+import {safeAreaMetrics} from '../testUtils';
+import NavigationDrawer from './NavigationDrawer';
+
+jest.mock('../data/token', () => ({useToken: jest.fn()}));
+
+describe('NavigationDrawer', () => {
+  describe('when signed in', () => {
+    it('allows signing out', async () => {
+      const navigation = {
+        navigate: jest.fn().mockName('navigation.navigate'),
+      };
+      const state = {
+        routes: [],
+      };
+      const clearToken = jest.fn().mockName('clearToken');
+      useToken.mockReturnValue({
+        isLoggedIn: true,
+        clearToken,
+      });
+
+      const {getByText} = render(
+        <SafeAreaProvider initialMetrics={safeAreaMetrics}>
+          <NavigationDrawer navigation={navigation} state={state} />
+        </SafeAreaProvider>,
+      );
+
+      fireEvent.press(getByText('Sign out'));
+
+      expect(clearToken).toHaveBeenCalledWith();
+
+      await waitFor(() =>
+        expect(navigation.navigate).toHaveBeenCalledWith('Sign in'),
+      );
+    });
+  });
+});
