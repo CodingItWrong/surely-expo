06cedbbc1a1845a2d02ceb766a38060dec322b97
Josh Justice <me@codingitwrong.com>
2022-05-21 20:30:18 -0400

    Update available to nock

--- src/screens/TodoList/Available.spec.js
+++ src/screens/TodoList/Available.spec.js
@@ -1,15 +1,10 @@
 import {useLinkTo} from '@react-navigation/native';
 import {fireEvent, render, waitFor} from '@testing-library/react-native';
+import nock from 'nock';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import authenticatedHttpClient from '../../data/authenticatedHttpClient';
 import {TokenProvider} from '../../data/token';
 import {mockUseFocusEffect, safeAreaMetrics} from '../../testUtils';
 import Available from './Available';
-
-jest.mock('../../data/authenticatedHttpClient', () => ({
-  __esModule: true,
-  default: jest.fn(),
-}));
 
 jest.mock('@react-navigation/native', () => ({
   useFocusEffect: jest.fn(),
@@ -47,10 +42,9 @@ describe('Available', () => {
 
   describe('when there is an error loading todos', () => {
     it('shows an error message', async () => {
-      const client = {
-        get: jest.fn().mockRejectedValue(),
-      };
-      authenticatedHttpClient.mockReturnValue(client);
+      nock('http://localhost:3000')
+        .get('/todos?filter[status]=available&include=category')
+        .reply(500, {});
 
       const {findByText} = render(
         <SafeAreaProvider initialMetrics={safeAreaMetrics}>
@@ -71,10 +65,9 @@ describe('Available', () => {
         included: [],
       };
 
-      const client = {
-        get: jest.fn().mockResolvedValue({data: response}),
-      };
-      authenticatedHttpClient.mockReturnValue(client);
+      nock('http://localhost:3000')
+        .get('/todos?filter[status]=available&include=category')
+        .reply(200, response);
 
       const {findByText} = render(
         <SafeAreaProvider initialMetrics={safeAreaMetrics}>
@@ -95,11 +88,9 @@ describe('Available', () => {
     };
 
     function renderComponent() {
-      const client = {
-        get: jest.fn().mockResolvedValue({data: response}),
-        post: jest.fn(),
-      };
-      authenticatedHttpClient.mockReturnValue(client);
+      const mockedServer = nock('http://localhost:3000')
+        .get('/todos?filter[status]=available&include=category')
+        .reply(200, response);
 
       const {findByText, getByLabelText, queryByText} = render(
         <SafeAreaProvider initialMetrics={safeAreaMetrics}>
@@ -109,18 +100,14 @@ describe('Available', () => {
         </SafeAreaProvider>,
       );
 
-      return {client, findByText, getByLabelText, queryByText};
+      return {findByText, getByLabelText, mockedServer, queryByText};
     }
 
     it('displays available todos from the server', async () => {
-      const {client, findByText, queryByText} = renderComponent();
+      const {findByText, queryByText} = renderComponent();
 
       await findByText(`${category.attributes.name} (1)`);
       expect(queryByText(todo.attributes.name)).not.toBeNull();
-
-      expect(client.get).toHaveBeenCalledWith(
-        'todos?filter[status]=available&include=category',
-      );
     });
 
     it('allows navigating to a todo detail', async () => {
@@ -138,27 +125,30 @@ describe('Available', () => {
       const todoName = 'My New Todo';
 
       it('allows adding a todo', async () => {
-        const {client, findByText, getByLabelText} = renderComponent();
-        client.post.mockResolvedValue({data: {}});
+        const {findByText, getByLabelText, mockedServer} = renderComponent();
 
         await findByText('Todo 1');
+
+        mockedServer
+          .post('/todos?', {
+            data: {type: 'todos', attributes: {name: todoName}},
+          })
+          .reply(200, {data: {}})
+          .get('/todos?filter[status]=available&include=category')
+          .reply(200, response);
 
         const addField = getByLabelText('New todo name');
         fireEvent.changeText(addField, todoName);
         fireEvent(addField, 'submitEditing');
 
-        expect(client.post).toHaveBeenCalledWith(
-          'todos?',
-          {data: {type: 'todos', attributes: {name: todoName}}},
-          {headers: {'Content-Type': 'application/vnd.api+json'}},
-        );
-
-        await waitFor(() => expect(client.get).toHaveBeenCalledTimes(2));
+        // confirm post and second get are sent
+        await waitFor(() => expect(mockedServer.isDone()).toBe(true));
       });
 
       it('shows an error when adding a todo fails', async () => {
-        const {client, findByText, getByLabelText} = renderComponent();
-        client.post.mockRejectedValue();
+        const {findByText, getByLabelText, mockedServer} = renderComponent();
+
+        mockedServer.post('/todos?').reply(500, {});
 
         await findByText('Todo 1');
 
