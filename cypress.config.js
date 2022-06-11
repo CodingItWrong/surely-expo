0b59f56e87b3b9a31bff181b0fc07fe7ba663d2b
Josh Justice <me@codingitwrong.com>
2022-06-11 07:39:13 -0400

    Config update

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,9 @@
-module.exports = {
+const {defineConfig} = require('cypress');
+
+module.exports = defineConfig({
   chromeWebSecurity: false,
   video: false,
-
   e2e: {
     baseUrl: 'http://localhost:19006',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
-};
+});
