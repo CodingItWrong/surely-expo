40523f6f82492e50be52ee7854fef5f2df74fe4b
Josh Justice <me@codingitwrong.com>
2022-06-25 08:04:03 -0400

    Update sign up to screen API

--- src/screens/Login/SignUpForm.spec.js
+++ src/screens/Login/SignUpForm.spec.js
@@ -1,5 +1,10 @@
 import {useLinkTo} from '@react-navigation/native';
-import {fireEvent, render, waitFor} from '@testing-library/react-native';
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react-native';
 import nock from 'nock';
 import {TokenProvider} from '../../data/token';
 import SignUpForm from './SignUpForm';
@@ -13,17 +18,13 @@ describe('SignUpForm', () => {
     const linkTo = jest.fn();
     useLinkTo.mockReturnValue(linkTo);
 
-    const {findByText, getByLabelText, getByText, queryByText} = render(
+    render(
       <TokenProvider loadToken={false}>
         <SignUpForm />
       </TokenProvider>,
     );
 
     return {
-      findByText,
-      getByLabelText,
-      getByText,
-      queryByText,
       linkTo,
     };
   }
@@ -36,18 +37,18 @@ describe('SignUpForm', () => {
       .post('/users?', {data: {type: 'users', attributes: {email, password}}})
       .reply(200, {data: {}});
 
-    const {findByText, getByLabelText, getByText, linkTo} = setUp();
+    const {linkTo} = setUp();
 
-    fireEvent.changeText(getByLabelText('Email'), email);
-    fireEvent.changeText(getByLabelText('Password'), password);
-    fireEvent.changeText(getByLabelText('Confirm password'), password);
-    fireEvent.press(getByText('Sign up'));
+    fireEvent.changeText(screen.getByLabelText('Email'), email);
+    fireEvent.changeText(screen.getByLabelText('Password'), password);
+    fireEvent.changeText(screen.getByLabelText('Confirm password'), password);
+    fireEvent.press(screen.getByText('Sign up'));
 
-    await findByText(
+    await screen.findByText(
       'Sign up successful. Sign in with the email and password you used to sign up.',
     );
 
-    fireEvent.press(getByText('Go to sign in'));
+    fireEvent.press(screen.getByText('Go to sign in'));
 
     expect(linkTo).toHaveBeenCalledWith('/signin');
 
@@ -58,30 +59,35 @@ describe('SignUpForm', () => {
     const email = 'example@example.com';
     const password = 'password';
 
-    const {getByLabelText, getByText, queryByText} = setUp();
+    setUp();
 
-    const signUpButton = getByText('Sign up');
+    const signUpButton = screen.getByText('Sign up');
     fireEvent.press(signUpButton);
 
-    expect(queryByText('Email is required.')).not.toBeNull();
+    expect(screen.queryByText('Email is required.')).not.toBeNull();
 
-    fireEvent.changeText(getByLabelText('Email'), email);
+    fireEvent.changeText(screen.getByLabelText('Email'), email);
     fireEvent.press(signUpButton);
-    expect(queryByText('Password is required.')).not.toBeNull();
+    expect(screen.queryByText('Password is required.')).not.toBeNull();
 
-    fireEvent.changeText(getByLabelText('Password'), password);
+    fireEvent.changeText(screen.getByLabelText('Password'), password);
     fireEvent.press(signUpButton);
-    expect(queryByText('Password confirmation is required.')).not.toBeNull();
+    expect(
+      screen.queryByText('Password confirmation is required.'),
+    ).not.toBeNull();
 
-    fireEvent.changeText(getByLabelText('Confirm password'), 'incorrect');
+    fireEvent.changeText(
+      screen.getByLabelText('Confirm password'),
+      'incorrect',
+    );
     fireEvent.press(signUpButton);
-    expect(queryByText('Passwords do not match.')).not.toBeNull();
+    expect(screen.queryByText('Passwords do not match.')).not.toBeNull();
   });
 
   it('allows cancelling signup', async () => {
-    const {getByText, linkTo} = setUp();
+    const {linkTo} = setUp();
 
-    fireEvent.press(getByText('Cancel'));
+    fireEvent.press(screen.getByText('Cancel'));
 
     await waitFor(() => {
       expect(linkTo).toHaveBeenCalledWith('/signin');
