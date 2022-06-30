f9db379a3781a847c8b6c51ab6e40f0da6844d8c
Josh Justice <me@codingitwrong.com>
2022-06-30 08:44:38 -0400

    Remove expo-app-loading

--- src/components/TokenLoadBuffer.js
+++ src/components/TokenLoadBuffer.js
@@ -1,11 +1,10 @@
-import AppLoading from 'expo-app-loading';
-import {useToken} from '../data/token';
+// import {useToken} from '../data/token';
 
 export default function TokenLoadBuffer({children}) {
-  const {isTokenLoaded} = useToken();
-  if (!isTokenLoaded) {
-    return <AppLoading />;
-  } else {
-    return children;
-  }
+  // const {isTokenLoaded} = useToken();
+  // if (!isTokenLoaded) {
+  //   return <AppLoading />;
+  // } else {
+  return children;
+  // }
 }
