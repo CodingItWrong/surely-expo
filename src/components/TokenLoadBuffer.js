37bbd5465730f94e29372821d2647f7bdebbe2f6
Josh Justice <me@codingitwrong.com>
2022-07-03 07:34:14 -0400

    Use SplashScreen API to wait for loading

--- src/components/TokenLoadBuffer.js
+++ src/components/TokenLoadBuffer.js
@@ -1,12 +1,27 @@
+import * as SplashScreen from 'expo-splash-screen';
+import {useCallback} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {useToken} from '../data/token';
 
 export default function TokenLoadBuffer({children}) {
   const {isTokenLoaded} = useToken();
+
+  // see https://docs.expo.dev/versions/latest/sdk/splash-screen/
+  const onLayoutRootView = useCallback(() => {
+    if (isTokenLoaded) {
+      return SplashScreen.hideAsync();
+    }
+  }, [isTokenLoaded]);
+
   if (!isTokenLoaded) {
-    return null;
+    SplashScreen.preventAutoHideAsync();
+    return null; // because children will error
   } else {
-    return <View style={styles.fill}>{children}</View>;
+    return (
+      <View style={styles.fill} onLayout={onLayoutRootView}>
+        {children}
+      </View>
+    );
   }
 }
 
