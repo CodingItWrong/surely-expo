45825a7d938dc519213fe4dfb50c3995b62d3502
Josh Justice <me@codingitwrong.com>
2022-07-04 09:28:16 -0400

    Set commented-out local ip

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,6 +1,13 @@
 import {Platform} from 'react-native';
 
+// ifconfig | grep "inet "
+// const LOCAL_IP = '10.0.1.52';
+
 function getBaseUrl() {
+  if (typeof LOCAL_IP !== 'undefined') {
+    return `http://${LOCAL_IP}:3000`;
+  }
+
   if (__DEV__) {
     if (Platform.OS === 'android') {
       return 'http://10.0.2.2:3000';
