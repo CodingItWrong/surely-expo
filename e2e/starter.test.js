9a40950f9eec2dc97c65cc5524205a650e79676b
Josh Justice <me@codingitwrong.com>
2023-07-26 07:20:43 -0400

    Set up smoke test

--- e2e/starter.test.js
+++ e2e/starter.test.js
@@ -1,4 +1,4 @@
-describe('Example', () => {
+describe('App', () => {
   beforeAll(async () => {
     await device.launchApp();
   });
@@ -7,17 +7,7 @@ describe('Example', () => {
     await device.reloadReactNative();
   });
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
-  });
-
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
-
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+  it('should show the hello message', async () => {
+    await expect(element(by.id('hello'))).toBeVisible();
   });
 });
