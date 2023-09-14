3358a1f1ad9baa171e82f54382b8135410dc2663
Josh Justice <me@codingitwrong.com>
2023-09-14 06:47:09 -0400

    Test creating and editing a todo

--- e2e/02-play-around.test.js
+++ e2e/02-play-around.test.js
@@ -20,5 +20,18 @@ describe('02 - Play Around Test', () => {
     await expect(
       element(by.text('You have no available todos. Nice work!')),
     ).toBeVisible();
+    await element(by.id('new-todo-name')).typeText('Clean office desk');
+    await element(by.id('new-todo-name')).tapReturnKey();
+
+    await expect(element(by.text('Clean office desk'))).toBeVisible();
+
+    await element(by.text('Clean office desk')).tap();
+    await element(by.id('edit-button')).tap();
+
+    await element(by.id('name-field')).typeText(' again');
+    await element(by.id('save-button')).tap();
+
+    await expect(element(by.id('save-button'))).not.toBeVisible();
+    await expect(element(by.text('Clean office desk again'))).toBeVisible();
   });
 });
--- src/data/authenticatedHttpClient.mock.js
+++ src/data/authenticatedHttpClient.mock.js
@@ -1,7 +1,55 @@
+const todos = [];
+let nextId = 1;
+
 export default function authenticatedHttpClient() {
   return {
-    get() {
-      return Promise.resolve({data: {}});
+    async get(url) {
+      if (url.startsWith('todos?')) {
+        return {
+          data: {
+            data: todos,
+          },
+        };
+      } else if (url.startsWith('todos/')) {
+        return {
+          data: {
+            data: todos[0],
+          },
+        };
+      } else {
+        console.log(`unmocked url: ${url}`);
+        return {data: {}};
+      }
+    },
+
+    async post(url, body) {
+      console.log(url, body);
+
+      if (url === 'todos?') {
+        const newTodo = {
+          id: nextId,
+          attributes: body.data.attributes,
+          relationships: {category: {}},
+        };
+        nextId += 1;
+        todos.push(newTodo);
+        return {data: newTodo};
+      } else {
+        return {data: {}};
+      }
+    },
+
+    async patch(url, body) {
+      if (url.startsWith('todos/')) {
+        todos[0].attributes = body.data.attributes;
+        return {
+          data: {
+            data: todos[0],
+          },
+        };
+      } else {
+        return {data: {}};
+      }
     },
   };
 }
