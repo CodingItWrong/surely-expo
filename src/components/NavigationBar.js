02c3cac2451bd4627bc01b4292916557f18e920e
Josh Justice <josh.justice2@cfacorp.com>
2024-07-01 05:43:14 -0400

    Color the header in light mode

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,14 +1,22 @@
-import {Appbar} from 'react-native-paper';
+import {Appbar, useTheme} from 'react-native-paper';
 import {large, useBreakpoint} from '../breakpoints';
+import {useColorScheme} from 'react-native';
 
 const showDrawerToggleForBreakpoint = breakpoint => breakpoint !== large;
 
 export default function NavigationBar({navigation, options, back}) {
   const breakpoint = useBreakpoint();
   const showDrawerToggle = showDrawerToggleForBreakpoint(breakpoint);
+  const colorScheme = useColorScheme() ?? 'light';
+  const theme = useTheme();
+
+  const lightHeaderStyle = {
+    backgroundColor: theme.colors.primaryContainer,
+  };
+  const headerStyle = colorScheme === 'light' ? lightHeaderStyle : null;
 
   return (
-    <Appbar.Header>
+    <Appbar.Header style={headerStyle}>
       {back ? (
         <Appbar.BackAction
           testID="back-button"
