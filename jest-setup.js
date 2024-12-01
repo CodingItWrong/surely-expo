06695fdd3decbe8872a6ab69da9d955fb4aefd98
Josh Justice <me@codingitwrong.com>
2024-12-01 17:03:52 -0500

    Fix or remove failing Jest tests

--- jest-setup.js
+++ jest-setup.js
@@ -1,8 +1,2 @@
-import {en, registerTranslation} from 'react-native-paper-dates';
-
-// Fixes "[react-native-paper-dates] en is not registered"
-registerTranslation('en', en);
-
-// Fixes "Animated: `useNativeDriver` is not supported because the native animated module is missing."
 // eslint-disable-next-line no-undef
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('expo-font');
